790ce82c456baa3990ae0b02f7eec3d6af75e84d
davbrican <72500941+davbrican@users.noreply.github.com>
2021-07-15 19:59:15 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,30 +3,45 @@ var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
 
+var localHostURL = "http://localhost:3000";
+
 // TODO Comprobar rutas del back-end
-describe('Testing assert function: ', function() {
-  describe('Check addTest Function', function(){
-    it('Check the returned value using : assert.equal(value, value): ', function() {
-       result = 2;
-       assert.equal(result, 2);
+
+  describe('Testing back-end routes: ', function() {
+
+
+    describe('./admin.js', function(){
+
+
+      it('Test post works: ', function() {
+        var session = {
+          name: "Test Session",
+          tokenPairing: false,
+          tokens: ["uno"]
+        }
+
+        fetch(localHostURL+'/sessions', {
+          method: "POST",
+          headers: {
+            Authorization: localStorage.adminSecret,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(session),
+        }).then(response => {
+          assert.strictEqual(response.status, 200);
+        }).catch(err => {
+          assert.fail('Error on request');
+        });
+      });
+
+
+      it('Test 2 ', function() {
+        assert.strictEqual(2,2);
+      });
+
+
     });
-  });
-})
 
-describe('Testing should function: ', function() {
-    describe('Check addTest Function', function(){
-      it('Check the returned value using : result.should.be.equal(value): ', function() {
-         result = 2;
-         result.should.be.equal(2);
-      })
-    })
-  })
 
-  describe('Testing expect function: ', function() {
-    describe('Check addTest Function', function(){
-      it('Check the returned value using : expect(result).to.be.a(value);: ', function() {
-         result = 2;
-          expect(result).to.equal(3);
-      })
-    })
-  })
+  }); 
+

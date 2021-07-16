acf5b357449e9edb606cfc3d7c4723c120247664
davbrican <72500941+davbrican@users.noreply.github.com>
2021-07-16 11:30:17 +0200

    tests admin.js (not all)

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
-var chai = require('chai');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
 var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
+const axios = require("axios");
 
+chai.use(chaiHttp);
 var localHostURL = "http://localhost:3000";
 
 // TODO Comprobar rutas del back-end
@@ -10,34 +13,151 @@ var localHostURL = "http://localhost:3000";
   describe('Testing back-end routes: ', function() {
 
 
-    describe('./admin.js', function(){
+    describe('./admin.js --> Sessions', function(){
 
 
-      it('Test post works: ', function() {
-        var session = {
-          name: "Test Session",
-          tokenPairing: false,
-          tokens: ["uno"]
-        }
-
-        fetch(localHostURL+'/sessions', {
-          method: "POST",
-          headers: {
-            Authorization: localStorage.adminSecret,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(session),
-        }).then(response => {
-          assert.strictEqual(response.status, 200);
-        }).catch(err => {
-          assert.fail('Error on request');
+      it('Get all sessions must be 200', (done) => {
+        chai.request(localHostURL)
+        .get('/sessions')
+        .set('Authorization', '300OT0n3l4d45')
+        .send()
+        .end( function(err,res){
+          expect(res).to.have.status(200);
+          done();
         });
       });
 
 
-      it('Test 2 ', function() {
-        assert.strictEqual(2,2);
-      });
+        it('Post a new session must be 200', (done) => {
+          chai.request(localHostURL)
+          .post('/sessions')
+          .set('Authorization', '300OT0n3l4d45')
+          .send({
+            name: "Test Session",
+            tokenPairing: false,
+            tokens: ["uno"]
+          })
+          .end( function(err,res){
+            expect(res).to.have.status(200);
+            done();
+          });
+        });
+
+
+        it('Get a session must be 200', (done) => {
+          chai.request(localHostURL)
+          .get('/sessions/Test Session')
+          .set('Authorization', '300OT0n3l4d45')
+          .send()
+          .end( function(err,res){
+            expect(res).to.have.status(200);
+            done();
+          });
+        });
+
+
+        it('Get a session doesnt exists must be 404', (done) => {
+          chai.request(localHostURL)
+          .get('/sessions/Session doesnt exists')
+          .set('Authorization', '300OT0n3l4d45')
+          .send()
+          .end( function(err,res){
+            expect(res).to.have.status(404);
+            done();
+          });
+        });
+
+
+        it('Post a new session with bad credentials must be 401', (done) => {
+          chai.request(localHostURL)
+          .post('/sessions')
+          .set('Authorization', 'an0th3rW0rd')
+          .send({
+            name: "Test Session",
+            tokenPairing: false,
+            tokens: ["uno"]
+          })
+          .end( function(err,res){
+            expect(res).to.have.status(401);
+            done();
+          });
+        }); 
+ 
+
+        it('Starts session must be 200', (done) => {
+          chai.request(localHostURL)
+          .post('/startSession/Test Session')
+          .send({
+            name: "Test Session"
+          })
+          .set('Authorization', '300OT0n3l4d45')
+          .end( function(err,res){
+            expect(res).to.have.status(200);
+            done();
+          });
+        });
+ 
+
+        // TODO Averiguar que hace status y hacer el test correspondiente
+        /*
+        it('Session status must be active', (done) => {
+          chai.request(localHostURL)
+          .get('/status/Test Session')
+          .send({
+            name: "Test Session"
+          })
+          .set('Authorization', '300OT0n3l4d45')
+          .end( function(err,res){
+            console.log(res.body)
+            done();
+          });
+        });
+        */
+       
+
+        it('Resets session must be 200', (done) => {
+          chai.request(localHostURL)
+          .post('/resetSession')
+          .send({
+            name: "Test Session"
+          })
+          .set('Authorization', '300OT0n3l4d45')
+          .end( function(err,res){
+            expect(res).to.have.status(200);
+            done();
+          });
+        });
+
+
+        // TODO hacer funcionar este test
+        /*
+        it('ToggleActivation in session must change', (done) => {
+          chai.request(localHostURL)
+          .put('/resetSession/Test Session/toggleActivation')
+          .send({
+            name: "Test Session"
+          })
+          .set('Authorization', '300OT0n3l4d45')
+          .end( function(err,res){
+            console.log(res)
+            done();
+          });
+        });
+        */
+
+
+        it('Deleting a session must be 200', (done) => {
+          chai.request(localHostURL)
+          .delete('/sessions/Test Session')
+          .send({
+            name: "Test Session"
+          })
+          .set('Authorization', '300OT0n3l4d45')
+          .end( function(err,res){
+            expect(res).to.have.status(200);
+            done();
+          });
+        });
 
 
     });

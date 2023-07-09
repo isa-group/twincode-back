333d8b1053be14c85a985719eeb1272ffd7fb345
Rafael González Castillero <rafaelgonzalezcastillero@gmail.com>
2023-07-09 20:32:22 +0200

    Fix bug and export/send mail functionalities

--- routes/participants.js
+++ routes/participants.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Test = require("../models/Test.js");
 const Logger = require("../logger.js");
 const User = require("../models/User.js");
+const nodemailer = require("nodemailer");
 
 router.get("/participants/:sessionName", (req, res) => {
   const adminSecret = req.headers.authorization;
@@ -35,7 +36,7 @@ router.get("/participants/:sessionName", (req, res) => {
             });
             res.send(orderedUsers);
           } else {
-            res.sendStatus(404);
+            res.send([]);
           }
         })
         .catch((error) => {
@@ -94,15 +95,108 @@ router.post("/participants/:sessionName/import", (req, res) => {
         participant.gender = user.gender;
         participant.shown_gender = user.gender;
         participant.birthDate = user.birthday;
-        participant.knownLanguages = user.knownLanguages;
         participant.beganStudying = user.studyStartYear;
-        participant.numberOfSubjects = user.subjectsNumber;
 
         participants.push(participant);
       });
 
       User.insertMany(participants);
       res.sendStatus(200);
+    } catch (e) {
+      Logger.monitorLog(e);
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+router.get("/participants/:sessionName/export", (req, res) => {
+  const adminSecret = req.headers.authorization;
+
+  if (adminSecret === process.env.ADMIN_SECRET) {
+    try {
+      User.find(
+        {
+          environment: process.env.NODE_ENV,
+          subject: req.params.sessionName,
+        },
+        {
+          _id: 0,
+          code: 1,
+          firstName: 1,
+          surname: 1,
+          mail: 1,
+          gender: 1,
+          birthDate: 1,
+          beganStudying: 1,
+        }
+      ).then((users) => {
+        Logger.monitorLog("Exporting participants");
+        console.log(users);
+        if (users.length > 0) {
+          res.send(users);
+        }
+      });
+    } catch (e) {
+      Logger.monitorLog("Error retrieving users... "+e);
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+router.post("/participants/:sessionName/:mail/send", (req, res) => {
+  const adminSecret = req.headers.authorization;
+
+  if (adminSecret === process.env.ADMIN_SECRET) {
+    try {
+      User.findOne({
+        environment: process.env.NODE_ENV,
+        subject: req.params.sessionName,
+        mail: req.params.mail,
+      }).then((user) => {
+        if (user) {
+          let transporter = nodemailer.createTransport({
+            host: "mail.us.es",
+            requierTLS: true,
+            port: 587,
+            auth: {
+              user: process.env.MAIL_USER,
+              pass: process.env.MAIL_PASS,
+            },
+          });
+
+          transporter.verify((err, success) => {
+            if (err) Logger.monitorLog("Error verifying transporter: "+err);
+            else Logger.monitorLog('Your config is correct');
+          });
+
+          transporter.sendMail({
+            from: '"TwinCode Team 👩🏼‍💻👨🏽‍💻" <no-reply@twincode.com>',
+            to: user.mail, // list of receivers
+            subject: "TwinCode - Your code", // Subject line
+            text: "Welcome to TwinCode", // plain text body
+            html: `
+              <h1>Welcome to TwinCode</h1>
+              <br/>
+              <p>Your anonymous code to participate in the twincode session is the following: <b>${user.code}</b></p>
+              <p>But you can click directly <a href="https://twincode.netlify.app/?code=${user.code}">HERE</a> for easy access when the session starts.</p><br/>
+              <p>You will receive detailed instructions at the beginning of the session.</p>`, // html body
+          }).then((info) => {
+            Logger.monitorLog("Message sent: " + info.messageId);
+            res.sendStatus(200);
+
+            transporter.close();
+            }).catch((error) => {
+              Logger.monitorLog("Error sending mail: "+error);
+              res.sendStatus(500);
+          });
+        } else {
+          res.status(404).send("Participant not found!");
+        }
+      });
     } catch (e) {
       Logger.monitorLog(e);
       res.sendStatus(500);
